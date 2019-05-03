31e9bd3816599034d8b98919a8282d9817bb6638
ehabrasul <Ehab.ohio@gmail.com>
2019-05-02 21:19:18 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+// Dependencies
+// ____________________________________________________________________________________
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+var routes = require("./controllers/burgers_controller");
+
+// Setup the Express App
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Setup the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Setup Handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Setup access to static assets
+app.use(express.static('public'))
+
+// Setup Routes
+app.use(routes);
+
+
+// Start server to listen to client requests.
+// ____________________________________________________________________________________
+
+app.listen(PORT, function() {
+  // Log (server-side) when the server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
